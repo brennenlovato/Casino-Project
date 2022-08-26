0fc82b7ca3b0432b9318fa39c7384f9243e34c0a
Brennen Lovato <brennenlovato@outlook.com>
2022-08-25 19:57:30 -0600

    created game menu and player class

---
+++ gamemenu.rb
@@ -0,0 +1,42 @@
+require_relative "player"
+
+class Casino
+  def initialize
+    @player == Player.new
+    show_menu
+  end
+
+  def show_menu
+    begin
+      puts "What do you want to play?"
+      puts "1) Rock, Paper, Scissors"
+      puts "2) Black Jack"
+      puts "3) Slots"
+      puts "4) Check Wallet"
+      puts "5) Exit"
+      response = gets.strip.to_i
+      raise "Bad Input" unless response > 0 && response < 6
+      case response
+      when 1
+        # todo: RPS
+      when 2
+        # todo: Black Jack
+      when 3
+        # todo: Slots
+      when 4
+        puts "You have $#{player.money}"
+      when 5
+        puts "Thanks for giving us your money!"
+        exit
+      else
+        raise
+      end
+      show_menu
+    rescue StandardError => e
+        puts e
+        retry
+    end
+  end
+end
+
+Casino.new
